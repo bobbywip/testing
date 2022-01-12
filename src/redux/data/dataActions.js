9d6bce40db254eb4c8666ac93943bd1d1b625fef
bobbywip <84804011+bobbywip@users.noreply.github.com>
2022-01-11 17:57:58 -0800

    Update dataActions.js

--- src/redux/data/dataActions.js
+++ src/redux/data/dataActions.js
@@ -27,7 +27,7 @@ export const fetchData = () => {
     try {
       let totalSupply = await store
         .getState()
-        .blockchain.smartContract.methods.totalSupply()
+        .blockchain.smartContract.methods.depositedTokens()
         .call();
       // let cost = await store
       //   .getState()
@@ -36,7 +36,7 @@ export const fetchData = () => {
 
       dispatch(
         fetchDataSuccess({
-          totalSupply,
+          depositedTokens,
           // cost,
         })
       );
